7f2dec49050db1e51e94416be77d1adf7847e62d
whjames007 <james.song007@hotmail.com>
2021-09-13 11:37:03 +0800

    20197提交
    20197提交

---
+++ java/whcdit-imengya/src/main/java/com/whcdit/imengya/WhcditImengyaApplication.java
@@ -0,0 +1,13 @@
+package com.whcdit.imengya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhcditImengyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhcditImengyaApplication.class, args);
+	}
+
+}
